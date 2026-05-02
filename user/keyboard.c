92dcf429c1a32cd8073c417fd1807e6e0c659ff8
cphillips68k-alt <cphillips68k@gmail.com>
2026-05-02 16:29:10 -0500

    keyboard.c fix

--- user/keyboard.c
+++ user/keyboard.c
@@ -1,30 +1,36 @@
-/* user/keyboard.c - Keyboard server (PID 2) */
+/* user/keyboard.c - Keyboard server (PID 2) with diagnostic */
 #include "common.h"
-
-static unsigned char sc_to_ascii(unsigned char sc) {
-    static const unsigned char table[] = 
-        "\0\x1b" "1234567890-=" "\b\t" "qwertyuiop[]" "\r"
-        "\0" "asdfghjkl;'`" "\0" "\\" "zxcvbnm,./" "\0" "*" "\0" " ";
-    if (sc < 58) return table[sc];
-    return 0;
-}
 
 void kbd_server_main(void) {
     unsigned int focus_pid = PID_SHELL;
 
-    /* Boot indicator - use direct putchar since we're just debugging */
+    /* Boot markers */
     putchar_at('K', 10);
     putchar_at('B', 12);
     putchar_at('D', 14);
+
+    /* Diagnostic counter — we'll place a star each time we wake up */
+    int wake_count = 0;
 
     while (1) {
         message_t msg;
         asm volatile("mov $3, %%eax; mov %0, %%ebx; int $0x80"
                      : : "r"(&msg) : "eax","ebx");
 
+        /* DIAGNOSTIC: draw a star to prove we woke up */
+        putchar_at('*', 20 + wake_count);
+        wake_count++;
+        if (wake_count > 10) wake_count = 0;   /* wrap */
+
+        /* Now handle the message */
         if (msg.type == 1) {
             unsigned char sc = msg.data[0];
-            unsigned char ascii = sc_to_ascii(sc);
+            /* translate scancode -> ASCII (simplified table) */
+            unsigned char ascii = 0;
+            static const unsigned char table[] =
+                "\0\x1b" "1234567890-=" "\b\t" "qwertyuiop[]" "\r"
+                "\0" "asdfghjkl;'`" "\0" "\\" "zxcvbnm,./" "\0" "*" "\0" " ";
+            if (sc < sizeof(table)) ascii = table[sc];
 
             if (ascii && focus_pid) {
                 message_t out;
