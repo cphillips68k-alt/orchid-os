d8ac1b736c12c373c1020fad3205e1e695d088ec
cphillips68k-alt <cphillips68k@gmail.com>
2026-05-02 16:17:16 -0500

    keyboard.c fix

--- user/keyboard.c
+++ user/keyboard.c
@@ -1,7 +1,6 @@
 /* user/keyboard.c - Keyboard server (PID 2) */
 #include "common.h"
 
-/* Scancode set 1 to ASCII (US layout) */
 static unsigned char sc_to_ascii(unsigned char sc) {
     static const unsigned char table[] = 
         "\0\x1b" "1234567890-=" "\b\t" "qwertyuiop[]" "\r"
@@ -13,24 +12,10 @@ static unsigned char sc_to_ascii(unsigned char sc) {
 void kbd_server_main(void) {
     unsigned int focus_pid = PID_SHELL;
 
-    /* Boot message via VGA */
-    message_t out;
-    out.type = 1;
-    out.data[0] = 'K';
-    *(unsigned int*)(out.data+1) = 10;
-    *(unsigned int*)(out.data+5) = 1;
-    asm volatile("mov $2, %%eax; mov %0, %%ebx; mov %1, %%ecx; int $0x80"
-                 : : "r"(PID_VGA), "r"(&out) : "eax","ebx","ecx");
-
-    out.data[0] = 'B';
-    *(unsigned int*)(out.data+1) = 11;
-    asm volatile("mov $2, %%eax; mov %0, %%ebx; mov %1, %%ecx; int $0x80"
-                 : : "r"(PID_VGA), "r"(&out) : "eax","ebx","ecx");
-
-    out.data[0] = 'D';
-    *(unsigned int*)(out.data+1) = 12;
-    asm volatile("mov $2, %%eax; mov %0, %%ebx; mov %1, %%ecx; int $0x80"
-                 : : "r"(PID_VGA), "r"(&out) : "eax","ebx","ecx");
+    /* Boot indicator - use direct putchar since we're just debugging */
+    putchar_at('K', 10);
+    putchar_at('B', 12);
+    putchar_at('D', 14);
 
     while (1) {
         message_t msg;
@@ -42,7 +27,7 @@ void kbd_server_main(void) {
             unsigned char ascii = sc_to_ascii(sc);
 
             if (ascii && focus_pid) {
-                /* Forward ASCII character to the focused process */
+                message_t out;
                 out.type = 1;
                 out.data[0] = ascii;
                 asm volatile("mov $2, %%eax; mov %0, %%ebx; mov %1, %%ecx; int $0x80"
