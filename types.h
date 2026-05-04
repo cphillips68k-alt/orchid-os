77c4f94ed6dbe61a04f72b77d08acd2b8e45e164
cphillips68k-alt <cphillips68k@gmail.com>
2026-05-04 16:46:10 -0500

    types.h fix

--- types.h
+++ types.h
@@ -16,4 +16,35 @@ typedef signed long long   s64;
 #define false 0
 #define NULL  ((void*)0)
 
+// x86 port I/O
+static inline u8 inb(u16 port) {
+    u8 val;
+    __asm__ volatile("inb %1, %0" : "=a"(val) : "Nd"(port));
+    return val;
+}
+
+static inline void outb(u16 port, u8 val) {
+    __asm__ volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline u16 inw(u16 port) {
+    u16 val;
+    __asm__ volatile("inw %1, %0" : "=a"(val) : "Nd"(port));
+    return val;
+}
+
+static inline void outw(u16 port, u16 val) {
+    __asm__ volatile("outw %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline u32 inl(u16 port) {
+    u32 val;
+    __asm__ volatile("inl %1, %0" : "=a"(val) : "Nd"(port));
+    return val;
+}
+
+static inline void outl(u16 port, u32 val) {
+    __asm__ volatile("outl %0, %1" : : "a"(val), "Nd"(port));
+}
+
 #endif
