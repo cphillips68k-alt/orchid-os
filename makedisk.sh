e3d58a953cf08dde261583ebf3bc6141fde3067b
cphillips68k-alt <cphillips68k@gmail.com>
2026-05-05 09:20:20 -0500

    makedisk.sh fix

--- makedisk.sh
+++ makedisk.sh
@@ -9,23 +9,23 @@ echo "Creating ${SIZE}MB disk image..."
 dd if=/dev/zero of=$DISK bs=1M count=$SIZE status=progress
 
 echo "Formatting as FAT32..."
-mkfs.fat -F 32 $DISK
+doas mkfs.fat -F 32 $DISK
 
 echo "Mounting..."
 mkdir -p $MNT
-sudo mount $DISK $MNT
+doas mount $DISK $MNT
 
 echo "Creating test files..."
-sudo sh -c 'echo "Hello from Orchid OS!" > '"$MNT"'/HELLO.TXT'
-sudo sh -c 'echo "This is a test file on Orchid OS." > '"$MNT"'/README.TXT'
-sudo sh -c 'echo "Orchid kernel is Ring 0." > '"$MNT"'/ORCHID.TXT'
-sudo mkdir -p "$MNT/TESTDIR"
-sudo sh -c 'echo "Nested file inside a directory." > '"$MNT"'/TESTDIR/NESTED.TXT'
-sudo mkdir -p "$MNT/APPS"
-sudo sh -c 'echo "Applications go here." > '"$MNT"'/APPS/INFO.TXT'
+doas sh -c 'echo "Hello from Orchid OS!" > '"$MNT"'/HELLO.TXT'
+doas sh -c 'echo "This is a test file on Orchid OS." > '"$MNT"'/README.TXT'
+doas sh -c 'echo "Orchid kernel is Ring 0." > '"$MNT"'/ORCHID.TXT'
+doas mkdir -p "$MNT/TESTDIR"
+doas sh -c 'echo "Nested file inside a directory." > '"$MNT"'/TESTDIR/NESTED.TXT'
+doas mkdir -p "$MNT/APPS"
+doas sh -c 'echo "Applications go here." > '"$MNT"'/APPS/INFO.TXT'
 
 echo "Unmounting..."
-sudo umount $MNT
+doas umount $MNT
 rmdir $MNT
 
 echo "Done. $DISK is ready."
